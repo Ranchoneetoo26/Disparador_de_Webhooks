74df6dc60ff78be3e171d9a536f5b1eaef177c50
Lucas Arruda <168601194+LucasUlissesArruda@users.noreply.github.com>
2025-10-04 18:19:41 -0300

    Reajustando Codigo da Main Para a Branch
    Apenas Adicionando oque não tinha

---
+++ models/softwarehouse.js
@@ -0,0 +1,42 @@
+'use strict';
+const {
+  Model
+} = require('sequelize');
+
+module.exports = (sequelize, DataTypes) => {
+  class SoftwareHouse extends Model {
+    static associate(models) {
+      
+    }
+  }
+  SoftwareHouse.init({
+    idSoftwareHouses: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      autoIncrement: true,
+      allowNull: false
+    },
+    data_criacao: {
+      type: DataTypes.DATE,
+      allowNull: false,
+      defaultValue: DataTypes.NOW
+    },
+    cnpj: {
+      type: DataTypes.STRING(14),
+      allowNull: false,
+      unique: true
+    },
+    token: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    status: {
+      type: DataTypes.STRING,
+      allowNull: false
+    }
+  }, {
+    sequelize,
+    modelName: 'SoftwareHouse',
+  });
+  return SoftwareHouse;
+};
