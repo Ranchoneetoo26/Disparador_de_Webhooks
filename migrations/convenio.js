b03924dbe4d98786b2338e882a0e767ce23fc34b
Lucas Arruda <168601194+LucasUlissesArruda@users.noreply.github.com>
2025-10-06 19:25:25 -0300

    Tabela Convenio em Models e Migration

---
+++ migrations/convenio.js
@@ -0,0 +1,36 @@
+'use strict';
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('Convenios', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      numero_convenio: {
+        allowNull: false,
+        type: Sequelize.STRING 
+      },
+      data_criacao: {
+        allowNull: false,
+        type: Sequelize.DATE,
+        defaultValue: Sequelize.literal('CURRENT_TIMESTAMP')
+      },
+      conta_id: {
+        allowNull: false, 
+        type: Sequelize.INTEGER,
+        references: {
+          model: 'Contas',
+          key: 'id'
+        },
+        onUpdate: 'CASCADE',
+        onDelete: 'RESTRICT' // não deixa apagar a conta se tiver um convenio
+      }
+    });
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('Convenios');
+  }
+};
