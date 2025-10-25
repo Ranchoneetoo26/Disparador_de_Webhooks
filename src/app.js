f7520a676cabc922b6cf705bcbe98334fd7b2599
antonio neto <antonioneetoo19@gmail.com>
2025-10-25 18:52:36 -0300

    configurando swagger, rodando

--- src/app.js
+++ src/app.js
@@ -2,6 +2,10 @@ import express from "express";
 import * as webhookRouterModule from "./infrastructure/http/express/routes/webhookRoutes.js";
 import protocoloRouter from "./infrastructure/http/express/routes/protocoloRoutes.js";
 
+import fs from "fs";
+import yaml from "js-yaml";
+import swaggerUi from "swagger-ui-express";
+
 const app = express();
 
 const webhookRouter = webhookRouterModule.default || webhookRouterModule;
@@ -15,8 +19,18 @@ app.get("/", (req, res) => {
 app.use("/webhooks", webhookRouter);
 app.use("/protocolo", protocoloRouter);
 
+try {
+  const swaggerDocument = yaml.load(
+    fs.readFileSync("./docs/Swagger.yml", "utf8")
+  );
+  app.use("/wb-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+} catch (e) {
+  console.error("Failed to load swagger.yml file:", e);
+}
+
 app.use((req, res, next) => {
   res.status(404).json({ error: "Endpoint not found" });
 });
 
+console.log("--- app.js: Fim da configuração ---"); // <-- Adicione aqui
 export default app;
